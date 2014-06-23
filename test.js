c447f635fb3d5f166265877877520770907b67c7
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-06-23 16:23:48 +0200

    Spaces

--- test.js
+++ test.js
@@ -14,14 +14,14 @@ it('should merge two arrays together', function () {
 });
 
 it('should merge two arrays together with undefined values', function () {
-	var result = arrayZip([1, 2], ['a', 'b', 'c']);
+  var result = arrayZip([1, 2], ['a', 'b', 'c']);
 
   assert.equal(result[0][0], 1);
   assert.equal(result[0][1], 'a');
 
   assert.equal(result[1][0], 2);
   assert.equal(result[1][1], 'b');
-	
+
   assert.equal(result[2][0], undefined);
   assert.equal(result[2][1], 'c');
 });
